c20facb75f9cc22b4350c66adb47f5b1c3d67d8d
Beyaz Zambak <v1kusen04ek@mail.ru>
2024-06-28 20:33:20 +0300

    Task 2

--- cook_book.py
+++ cook_book.py
@@ -11,6 +11,20 @@ def create_cook_book(recipes_file):
                 ingredient_dict = {'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure.strip()}
                 cook_book[dish_name].append(ingredient_dict)
             file.readline()
-    return print(cook_book)
+    return cook_book
 
-create_cook_book("recipes.txt")
+def get_shop_list_by_dishes(dishes, person_count):
+    cook_book = create_cook_book("recipes.txt")
+    dict_by_dishes = {}
+    for dish in dishes:
+        ingredients_list = cook_book.get(dish, "Такого рецепта нет")
+        for ingredient in ingredients_list:
+            if dict_by_dishes.get(ingredient['ingredient_name'], False) == False:
+                dict_by_dishes[ingredient['ingredient_name']] = {'measure': ingredient['measure'], 'quantity': int(ingredient['quantity']) * person_count}
+            else:
+                ingredient_name = dict_by_dishes[ingredient['ingredient_name']]
+                quantity = ingredient_name.pop('quantity', None)
+                dict_by_dishes[ingredient['ingredient_name']] = {'measure': ingredient['measure'], 'quantity': int(quantity) + int(ingredient['quantity']) * person_count}
+    return print(dict_by_dishes)
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2) 
